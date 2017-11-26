6c5e460486938ad9f4260fb231f9004cd4396e75
Sainath Balaguru Surender <sainath.revalsys@outlook.com>
2017-11-26 17:21:59 +0530

    Initial Push

---
+++ HellTrans.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+using System;
+namespace Core
+{
+    public static class HellTrans
+    {
+        public static string ToNumber(string raw)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HellTrans
+{
+    [Activity(Label = "HellTrans", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            EditText AccountNumberText = FindViewById<EditText>(Resource.Id.txtAccountNumber);
+            EditText IFSCCodeText = FindViewById<EditText>(Resource.Id.txtIFSCCode);
+            Button SaveButton = FindViewById<Button>(Resource.Id.btnSave);
+            Button ResetButton = FindViewById<Button>(Resource.Id.btnReset);
+            Button CancelButton = FindViewById<Button>(Resource.Id.btnCancel);
+
+            ResetButton.Click += (sender, e) =>
+            {
+                AccountNumberText.Text = string.Empty;
+                IFSCCodeText.Text = string.Empty;
+            };
+        }
+    }
+}
+
